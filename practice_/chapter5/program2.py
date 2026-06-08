95863ddc3e4ea91753235a2bf479a143f3fd06fc
prachibhardwaj460 <prachibhardwaj460@gmail.com>
2026-06-08 20:09:33 +0530

    Add files via upload

---
+++ practice_/chapter5/program2.py
@@ -0,0 +1,23 @@
+# Write a program to input eight numbers from the user and display all the unique numbers  (once)
+
+
+s = set()
+i = input("Enter your number: ")
+s.add(int(i))
+i = input("Enter your number: ")
+s.add(int(i))
+i = input("Enter your number: ")
+s.add(int(i))
+i = input("Enter your number: ")
+s.add(int(i))
+i = input("Enter your number: ")
+s.add(int(i))
+i = input("Enter your number: ")
+s.add(int(i))
+i = input("Enter your number: ")
+s.add(int(i))
+i = input("Enter your number: ")
+s.add(int(i))
+
+
+print(type(s))
